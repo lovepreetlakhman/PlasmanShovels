c08d3ef3d82760b35d48774f3eb3a44249a3b546
Lovepreet Singh <105748512+lovepreetlakhman@users.noreply.github.com>
2022-10-16 20:25:32 -0400

    updated migrations folder

---
+++ Migrations/20221015223958_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlasmanShovels.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Shovel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Company = table.Column<string>(nullable: true),
+                    Usage = table.Column<string>(nullable: true),
+                    Material = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    Type = table.Column<string>(nullable: true),
+                    Weight = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shovel", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shovel");
+        }
+    }
+}
